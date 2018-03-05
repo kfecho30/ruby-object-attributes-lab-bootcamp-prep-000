639e7272a6868e81bd1038c013314e79fb4d7cc9
Kelsey Fecho <kfecho30@gmail.com>
2018-03-05 02:25:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,17 @@ class Dog
     @name = name
     @breed = breed
   end
+
   def name
     @name
   end
+
   def breed
     @breed
   end
+
   def name=(new_name)
     @name = new_name
   end
+  
 end
